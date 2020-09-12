e41a0ef51c71c94fde78c972243e521017f10a71
allan02machado <70041162+allan02machado@users.noreply.github.com>
2020-09-11 22:11:45 -0300

    Adiciona resposta do exercício

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Cidades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guarujá");
+            Console.WriteLine("Santos");
+            Console.WriteLine("São Vicente");
+            Console.WriteLine("Praia Grande");
+            Console.WriteLine("Mongaguá");
+            Console.WriteLine("Itanhaém");
+            Console.WriteLine("Peruíbe");
+            Console.WriteLine("Iguape");
+            Console.WriteLine("Ilha Comprida");
+            Console.WriteLine("Cananéia");
+        }
+    }
+}
